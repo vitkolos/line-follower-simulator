254d934e957c97151a4b257cf5270fb876bff57b
vitkolos <vit.kolos@gmail.com>
2024-07-15 15:15:21 +0200

    Draft tests

---
+++ TestSuite/BoolBitmapTests.cs
@@ -0,0 +1,8 @@
+namespace TestSuite;
+
+public class BoolBitmapTests {
+    [Fact]
+    public void Test1() {
+
+    }
+}
---
+++ TestSuite/SimulatedRobotTests.cs
@@ -0,0 +1,18 @@
+namespace TestSuite;
+
+public class SimulatedRobotTests {
+    /*
+        should test (for a predefined fake/testing robot):
+        - buttons function
+        - leds and buttons
+        - position history
+            a) times (for varying intervals)
+            b) places (assuming we know how the robot should move)
+        - when setup and loop are called (and how many times)
+    */
+
+    [Fact]
+    public void Test1() {
+
+    }
+}
